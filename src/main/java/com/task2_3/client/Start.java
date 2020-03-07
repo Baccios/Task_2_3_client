772b1f9edd062d7aee3384fd1ce7f319072b939e
baccio <baccio.leonardo@gmail.com>
2020-03-07 16:10:42 +0100

    More tests added to the main function.

--- src/main/java/com/task2_3/client/Start.java
+++ src/main/java/com/task2_3/client/Start.java
@@ -8,7 +8,23 @@ public class Start {
         Admin_Protocol_Client client2 = new Admin_Protocol_Client("localhost",2020);
         Admin_Protocol_Client client3 = new Admin_Protocol_Client("localhost",2020);
         client2.startAuthHandshake("admin","ciaccio");
-        client3.startAuthHandshake("admin","ciaccio");
+        client3.startAuthHandshake("admin","fronchio");
         client.requestCheckout();
+        client.close();
+        client2.close();
+        client3.close();
+        client2 = new Admin_Protocol_Client("localhost",2020);
+        client2.startAuthHandshake("admin","patacchio"); //wrong credentials
+        client2.close();
+        client3 = new Admin_Protocol_Client("localhost",2020);
+        client3.startAuthHandshake("admin","ciaccio");
+        client3.requestCredentials("admin", "patacchio");
+        client3.requestScrape();
+        client3.requestCheckout();
+        client3.close();
+        client2 = new Admin_Protocol_Client("localhost",2020);
+        client2.startAuthHandshake("admin","patacchio");
+        client2.requestCheckout();
+        client2.close();
     }
 }

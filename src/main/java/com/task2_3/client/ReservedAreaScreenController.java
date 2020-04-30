84ddc97e46387f49fa736927ea084df1fbd58c28
zaccaria-essaid <zaccaria-essaid@hotmail.it>
2020-04-30 18:20:23 +0200

    Aggiunti grafici a torta nella finestra OverallStatistics

---
+++ src/main/java/com/task2_3/client/ReservedAreaScreenController.java
@@ -0,0 +1,14 @@
+package com.task2_3.client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReservedAreaScreenController {
+
+
+}
